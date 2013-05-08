ea40f86d0231588f54e7c37cd8feef605d78e50b
Damian Cholewa <damian.cholewa.choli@gmail.com>
2013-05-09 01:40:10 +0200

    Added modelnode.h

---
+++ modelnode.h
@@ -0,0 +1,18 @@
+#ifndef MODELNODE_H
+#define MODELNODE_H
+
+#include <QGLSceneNode>
+
+class modelNode : public QGLSceneNode
+{
+    Q_OBJECT
+public:
+    explicit modelNode(QObject *parent = 0);
+    
+signals:
+    
+public slots:
+    
+};
+
+#endif // MODELNODE_H
